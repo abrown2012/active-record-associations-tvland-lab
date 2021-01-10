1b09f70652415a9acb50838bdbe692a8c1d391f9
Agnieszka Brown <abrown2012@icloud.com>
2021-01-09 22:30:09 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+  def actors_list
+    list = []
+    self.actor_ids.each do |a|
+    list << "#{Actor.find(a).first_name} #{Actor.find(a).last_name}"
+    end
+    list 
+  end
 end
